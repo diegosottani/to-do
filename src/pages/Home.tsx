6131d6cf83ea00a0031259635b740d3116dff44f
Diego Sottani <diegosottani@gmail.com>
2022-01-26 09:40:40 -0300

    Desafio concluído

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const foundTask = updatedTasks.find(item => item.id === id)
+    if(!foundTask){
+      //Alert.alert('Nenhuma tarefa encontrada')
+      return
+    }
+    foundTask.done = !foundTask.done
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newState = tasks.filter(task => task.id !== id)
+    setTasks(newState)
   }
 
   return (
