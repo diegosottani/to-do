b5785df06e33ac5911bf917bccafb8073478016f
Diego <diegosottani@gmail.com>
2022-08-02 19:13:38 -0300

    Adicionando alertas e edição elaborada na aplicação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,20 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+type EditTask = {
+  taskId: number,
+  taskNewTitle: string
+}
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const taskRepeated = tasks.find(task => task.title === newTaskTitle)
+    if(taskRepeated){
+      return Alert.alert('Task já cadastrada', 'Você não pode cadastrar uma task com o mesmo nome')
+    }
+
     const newTask = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -22,7 +32,6 @@ export function Home() {
     const updatedTasks = tasks.map(task => ({ ...task }))
     const foundTask = updatedTasks.find(item => item.id === id)
     if(!foundTask){
-      //Alert.alert('Nenhuma tarefa encontrada')
       return
     }
     foundTask.done = !foundTask.done
@@ -30,8 +39,35 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    const newState = tasks.filter(task => task.id !== id)
-    setTasks(newState)
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: 'Não',
+          style: "cancel"
+        },
+        {
+          text: "OK", 
+          onPress: () => {
+            const newState = tasks.filter(task => task.id !== id)
+            setTasks(newState)
+          },
+          style: 'destructive'
+        }
+      ]
+    )    
+  }
+
+  function handleEditTask({taskId, taskNewTitle}: EditTask) {
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const taskToSet = updatedTasks.find(item => item.id === taskId)
+    if(!taskToSet){
+      return
+    }
+
+    taskToSet.title = taskNewTitle
+    setTasks(updatedTasks)
   }
 
   return (
@@ -43,7 +79,8 @@ export function Home() {
       <TasksList 
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
+        editTask={handleEditTask} 
       />
     </View>
   )
